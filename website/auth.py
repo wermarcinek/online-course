9409014e8cb44e31a08aaa7cb0d9e85ae6927151
wermarcinek <72862143+wermarcinek@users.noreply.github.com>
2023-08-13 16:01:51 +0200

    proszę działać ; - ;

---
+++ website/auth.py
@@ -0,0 +1,236 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+from .models import User, Points
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db  ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+from .questions import *
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/')
+def index():  # put application's code here
+    return redirect('/20_marcinek/licencjat/mainPage')
+
+@auth.route('/projektowanie')
+def projektowanie():
+    return render_template('projektowanie.html')
+
+@auth.route('/grafika')
+def grafika():
+    return render_template('grafika.html')
+
+@auth.route('/progress')
+@login_required
+def progress():
+    CU_name = current_user.first_name
+    x = None
+    tt1 = Points.query.filter_by(data='test1').join(User).filter_by(first_name=CU_name).order_by(
+        Points.points.desc()).first()
+    if (tt1 == x):
+        tt1 = 'Nie brałeś udziału'
+    else:
+        tt1 = tt1.points
+
+    tt2 = Points.query.filter_by(data='test2').join(User).filter_by(first_name=CU_name).order_by(
+        Points.points.desc()).first()
+    if (tt2 == x):
+        tt2 = 'Nie brałeś udziału'
+    else:
+        tt2 = tt2.points
+
+    return render_template('progress.html', tt1=tt1, tt2=tt2, user=current_user)
+
+
+@auth.route('/course')
+def course():
+    return render_template('course.html')
+
+@auth.route('/quizFig')
+def quizFig():
+    return render_template('quizFig.html')
+
+@auth.route('/quizProj')
+def quizProj():
+    return render_template('quizProj.html')
+
+@auth.route('/quizy')
+def quiz():
+    return render_template('quiz.html')
+
+
+@auth.route('/test', methods=['GET', 'POST'])
+def test():
+    nazwa='test1'
+    if 'question_index' not in session:
+        session['question_index'] = 0
+        session['points'] = 0
+
+    if request.method == 'POST':
+        odpowiedzi = request.form
+        current_question = DANE[session['question_index']]
+
+        if odpowiedzi.get('odpowiedz') == current_question['odpok']:
+            session['points'] += 1
+
+        session['question_index'] += 1
+
+        if session['question_index'] >= len(DANE):
+            points = session['points']
+            session.pop('question_index')
+            session.pop('points')
+
+            if current_user.is_authenticated:
+                new_points = Points(user_id=current_user.id, points=points, data=nazwa)  # Provide the user_id
+                db.session.add(new_points)
+                db.session.commit()
+
+            flash('Liczba poprawnych odpowiedzi, to: {0}'.format(points))
+            return redirect(url_for('auth.wynik'))
+
+    if 'question_index' in session:
+        current_question = DANE[session['question_index']]
+        return render_template('test.html', pytanie=current_question)
+
+    return redirect(url_for('/wynik'))
+
+@auth.route('/test2', methods=['GET', 'POST'])
+def test2():
+    nazwa='test2'
+    if 'question_index' not in session:
+        session['question_index'] = 0
+        session['points'] = 0
+
+    if request.method == 'POST':
+        odpowiedzi = request.form
+        current_question = DANE2[session['question_index']]
+
+        if odpowiedzi.get('odpowiedz') == current_question['odpok']:
+            session['points'] += 1
+
+        session['question_index'] += 1
+
+        if session['question_index'] >= len(DANE2):
+            points = session['points']
+            session.pop('question_index')
+            session.pop('points')
+
+            if current_user.is_authenticated:
+                new_points = Points(user_id=current_user.id, points=points, data=nazwa)  # Provide the user_id
+                db.session.add(new_points)
+                db.session.commit()
+
+            flash('Liczba poprawnych odpowiedzi, to: {0}'.format(points))
+            return redirect(url_for('auth.wynik'))
+
+    if 'question_index' in session:
+        current_question = DANE2[session['question_index']]
+        return render_template('test2.html', pytanie=current_question)
+
+    return redirect(url_for('/wynik'))
+
+@auth.route('/wynik')
+def wynik():
+    return render_template('wynik.html')
+
+
+###########################
+
+@auth.route('/admin')
+def admin():
+    return render_template('admin.html', user=current_user)
+
+
+@auth.route('/mainPage')
+def mainPage():
+    return render_template("mainPage.html", user=current_user)
+
+@auth.route('/contact')
+def contact():
+
+    return render_template("contact.html", user=current_user)
+
+@auth.before_request
+def restrict_access_to_auth_routes():
+    if current_user.is_authenticated and request.endpoint in ['auth.login', 'auth.sign_up']:
+        return redirect(url_for('auth.mainPage'))
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect('mainPage')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template('login.html', user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('auth.login'))
+
+    return render_template("signup.html", user=current_user)
+
+@auth.route('/changepassword', methods=['GET', 'POST'])
+@login_required
+def changepassword():
+    if request.method == 'POST':
+        old_password = request.form.get('old_password')
+        new_password1 = request.form.get('new_password1')
+        new_password2 = request.form.get('new_password2')
+
+        if not check_password_hash(current_user.password, old_password):
+            flash('Incorrect old password.', category='error')
+        elif new_password1 != new_password2:
+            flash('New passwords don\'t match.', category='error')
+        elif len(new_password1) < 7:
+            flash('New password must be at least 7 characters.', category='error')
+        else:
+            current_user.password = generate_password_hash(new_password1, method='sha256')
+            db.session.commit()
+            flash('Password changed successfully!', category='success')
+            return redirect(url_for('auth.mainPage'))
+
+    return render_template("changepassword.html", user=current_user)
---
+++ website/questions.py
@@ -0,0 +1,114 @@
+DANE = [{
+    'pytanie': 'Co to jest responsywność w kontekście projektowania stron internetowych?',  # pytanie
+    'odpowiedzi': ['Technika tworzenia stron bez wykorzystania kodu', 'Umiejętność pisania zaawansowanego JavaScriptu', 'Technika dostosowywania strony do różnych rozmiarów ekranów', 'Proces tworzenia animacji na stronie'],  # możliwe odpowiedzi
+    'odpok': 'Technika dostosowywania strony do różnych rozmiarów ekranów'},  # poprawna odpowiedź
+{
+        'pytanie': 'Jakie są trzy główne języki używane w projektowaniu stron internetowych?',
+        'odpowiedzi': ['HTML, C++, Java', 'Python, CSS, Ruby', 'HTML, CSS, JavaScript', 'PHP, SQL, C#'],
+        'odpok': 'HTML, CSS, JavaScript'},
+{
+        'pytanie': 'Co to jest CSS i w jaki sposób jest wykorzystywane w projektowaniu stron internetowych?',
+        'odpowiedzi': ['Język do tworzenia interaktywnych elementów strony', 'Język do zarządzania bazami danych', 'Język do tworzenia animacji', 'Język do określania stylów i wyglądu elementów HTML'],
+        'odpok': 'Język do określania stylów i wyglądu elementów HTML'},
+{
+        'pytanie': 'Dlaczego testowanie przeglądarkowe jest ważne w projektowaniu stron internetowych?',
+        'odpowiedzi': ['Nie jest ważne, wszystkie przeglądarki działają tak samo', 'Testowanie przeglądarkowe pomaga dostosować stronę do jednej przeglądarki', 'Pomaga upewnić się, że strona działa poprawnie na różnych przeglądarkach', 'Testowanie przeglądarkowe wpływa tylko na szybkość ładowania strony'],
+        'odpok': 'Pomaga upewnić się, że strona działa poprawnie na różnych przeglądarkach'},
+{
+        'pytanie': 'Jakie są zasady dostępności w projektowaniu stron internetowych?',
+        'odpowiedzi': ['Zasady związane tylko z bezpieczeństwem strony', 'Zasady dotyczące wyłącznie grafiki i animacji', 'Zasady tworzenia stron przyjaznych dla różnych grup użytkowników, w tym osób z niepełnosprawnościami', 'Zasady związane wyłącznie z optymalizacją dla wyszukiwarek'],
+        'odpok': 'Zasady tworzenia stron przyjaznych dla różnych grup użytkowników, w tym osób z niepełnosprawnościami'},
+{
+        'pytanie': 'Co to jest UX Design?',
+        'odpowiedzi': ['Ultra Xylophone Design', 'User Experience Design', 'Unusual XML Design', 'Universal X-Ray Design'],
+        'odpok': 'User Experience Design'},
+{
+        'pytanie': 'Co to jest "wireframe" w kontekście projektowania stron?',
+        'odpowiedzi': ['Ramka wykorzystywana do zawieszenia rysunków', 'Przewodnik po zastosowaniach frameworków', 'Szablon strony internetowej bez grafiki', 'Animowany efekt na stronie'],
+        'odpok': 'Szablon strony internetowej bez grafiki'},
+{
+        'pytanie': 'Co to jest "frontend" w projektowaniu stron internetowych?',
+        'odpowiedzi': ['Proces przesyłania plików na serwer', 'Strona internetowa ukierunkowana na użytkowników końcowych', 'Strona internetowa ukierunkowana na programistów', 'Proces optymalizacji bazy danych'],
+        'odpok': 'Strona internetowa ukierunkowana na użytkowników końcowych'},
+{
+        'pytanie': 'Co oznacza skrót UI w kontekście projektowania stron internetowych?',
+        'odpowiedzi': ['User Interface', 'Underlying Information', 'Universal Interaction', 'Unidentified Icon'],
+        'odpok': 'User Interface'},
+{
+        'pytanie': 'Co to jest projektowanie stron internetowych?',
+        'odpowiedzi': ['Proces tworzenia treści do serwisów społecznościowych', 'Tworzenie wyłącznie tekstu na stronie', 'Proces tworzenia estetycznych i funkcjonalnych witryn internetowych', 'Przesyłanie plików między komputerami'],
+        'odpok': 'Proces tworzenia estetycznych i funkcjonalnych witryn internetowych'},
+{
+        'pytanie': 'Dlaczego responsywność jest ważna w projektowaniu stron internetowych?',
+        'odpowiedzi': ['Nie ma wpływu na doświadczenie użytkownika', 'Ułatwia wyłącznie pracę programistów', 'Zapewnia spójny wygląd i funkcjonalność na różnych urządzeniach', 'Ogranicza dostęp do strony na telefonach komórkowych'],
+        'odpok': 'Zapewnia spójny wygląd i funkcjonalność na różnych urządzeniach'},
+{
+        'pytanie': 'Jakie są techniki stosowane do osiągnięcia responsywności strony?',
+        'odpowiedzi': ['Dodawanie jak największej ilości grafik', 'Używanie tylko jednej czcionki na stronie', 'Wykorzystywanie mediów z opóźnieniem', 'Używanie Media Queries w CSS'],
+        'odpok': 'Używanie Media Queries w CSS'},
+{
+        'pytanie': 'Co to są Media Queries w kontekście projektowania responsywnych stron?',
+        'odpowiedzi': ['Zapytania do bazy danych', 'Zapytania do serwera', 'Zapytania do użytkowników', 'Warunkowe instrukcje CSS, które dostosowują wygląd strony do różnych urządzeń'],
+        'odpok': 'Warunkowe instrukcje CSS, które dostosowują wygląd strony do różnych urządzeń'},
+{
+        'pytanie': 'Jakie są wyzwania związane z projektowaniem responsywnych stron?',
+        'odpowiedzi': ['Brak wyzwań, responsywność jest łatwa do osiągnięcia', 'Konieczność dostosowania treści do różnych rozmiarów ekranów', 'Konieczność używania tylko tekstu na stronie', 'Brak potrzeby testowania na różnych urządzeniach'],
+        'odpok': 'Konieczność dostosowania treści do różnych rozmiarów ekranów'},
+{
+        'pytanie': 'Co to jest "mobile-first design" w kontekście responsywności?',
+        'odpowiedzi': ['Tworzenie strony tylko dla komputerów', 'Projektowanie strony z myślą o urządzeniach mobilnych jako priorytet', 'Tworzenie strony tylko dla urządzeń mobilnych', 'Projektowanie strony bez responsywności'],
+        'odpok': 'Projektowanie strony z myślą o urządzeniach mobilnych jako priorytet'},
+{
+        'pytanie': 'Co to jest viewport w responsywnym projektowaniu?',
+        'odpowiedzi': ['Stylowy element strony', 'Okno przeglądarki, które wyświetla stronę', 'Animacja na stronie', 'Ukryty element strony'],
+        'odpok': 'Okno przeglądarki, które wyświetla stronę'},
+]
+DANE2 = [{
+        'pytanie': 'Jakie są zalecane punkty graniczne szerokości ekranu dla responsywności strony internetowej ekranu mobilnego?',
+        'odpowiedzi': ['do 768px', 'powyżej 768px', 'do 480px', 'powyżej 1200px'],
+        'odpok': 'do 480px'},
+{
+        'pytanie': 'Jaki jest przybliżony zakres rozmiarów czcionek dla nagłówków (H1-H6) na stronie internetowej?',
+        'odpowiedzi': ['10px - 14px', '16px - 18px', '20px - 24px', '28px - 32px'],
+        'odpok': '20px - 24px'},
+{
+        'pytanie': 'W jaki sposób można zoptymalizować obrazy na stronie internetowej pod kątem rozmiaru?',
+        'odpowiedzi': ['Używając tylko formatu GIF', 'Korzystając z formatów BMP i TIFF', 'Używając formatów JPEG i PNG oraz kompresji obrazów', 'Używając tylko formatu SVG'],
+        'odpok': 'Używając formatów JPEG i PNG oraz kompresji obrazów'},
+{
+        'pytanie': 'Jakie są zalecane punkty graniczne szerokości ekranu dla responsywności strony internetowej na tablety?',
+        'odpowiedzi': ['do 768px', 'powyżej 768px', 'do 480px', 'powyżej 1200px'],
+        'odpok': 'do 768px'},
+{
+        'pytanie': 'Jaka jest zalecana minimalna szerokość ekranu dla responsywności strony internetowej na desktopy?',
+        'odpowiedzi': ['do 768px', 'powyżej 768px', 'do 480px', 'powyżej 1200px'],
+        'odpok': 'powyżej 768px'},
+{
+        'pytanie': 'Jaka jest zalecana maksymalna szerokość zawartości strony dla ekranów desktopowych?',
+        'odpowiedzi': ['Do 1400px', 'Do 1600px', 'Do 2000px', 'Do 1200px'],
+        'odpok': 'Do 1200px'},
+{
+        'pytanie': 'Jakie są zalecane rozmiary czcionek dla tekstu podstawowego na stronie internetowej?',
+        'odpowiedzi': ['8px - 10px', '12px - 14px', '16px - 18px', '20px - 22px'],
+        'odpok': '16px - 18px'},
+{
+        'pytanie': 'Jakie są główne korzyści wynikające z odpowiedniego zastosowania marginesów i paddingów na stronie internetowej?',
+        'odpowiedzi': ['Poprawa pozycjonowania strony w wyszukiwarkach', 'Zwiększenie liczby interaktywnych elementów na stronie', 'Ułatwienie zarządzania treściami na stronie', 'Zapewnienie czytelności i estetyki poprzez właściwy układ i odstępy między elementami'],
+        'odpok': 'Zapewnienie czytelności i estetyki poprzez właściwy układ i odstępy między elementami'},
+{
+        'pytanie': 'Jaki jest zazwyczaj akceptowalny zakres rozmiarów plików graficznych dla mniejszych obrazów na stronie?',
+        'odpowiedzi': ['Powyżej 1 MB', 'Poniżej 100 KB', 'Poniżej 500 KB', 'Powyżej 2 MB'],
+        'odpok': 'Poniżej 100 KB'},
+{
+        'pytanie': 'Jakie są zalecane wartości rozmiarów plików graficznych dla większych obrazów na stronie?',
+        'odpowiedzi': ['Poniżej 100 KB', 'Powyżej 1 MB', 'Poniżej 500 KB', 'Powyżej 2 MB'],
+        'odpok': 'Poniżej 500 KB'},
+{
+        'pytanie': 'Dlaczego ważne jest zoptymalizowanie rozmiaru obrazów na stronie?',
+        'odpowiedzi': ['Obrazy nie mają wpływu na szybkość ładowania strony', 'Zoptymalizowane obrazy przyciągają większą liczbę użytkowników', 'Dla zapewnienia dobrego czasu ładowania strony oraz ograniczenia zużycia transferu danych', 'Rozmiar obrazów nie ma znaczenia dla funkcjonowania strony'],
+        'odpok': 'Dla zapewnienia dobrego czasu ładowania strony oraz ograniczenia zużycia transferu danych'},
+{
+        'pytanie': 'Co jest ważne przy określaniu ilości elementów interaktywnych na stronie?',
+        'odpowiedzi': ['Zawsze należy umieszczać jak najwięcej interaktywnych elementów dla lepszego zaangażowania użytkowników', 'Liczba elementów interaktywnych nie ma wpływu na doświadczenie użytkownika', 'Ilość elementów interaktywnych nie powinna przekraczać 10 na stronie', 'Liczba interaktywnych elementów powinna być dostosowana do celów strony, unikając przekroczenia granicy zatłoczenia'],
+        'odpok': 'Liczba interaktywnych elementów powinna być dostosowana do celów strony, unikając przekroczenia granicy zatłoczenia'},
+]
