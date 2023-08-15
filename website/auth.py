94310518e4fed0255bed810025d4431c6554afa5
wermarcinek <72862143+wermarcinek@users.noreply.github.com>
2023-08-15 21:08:39 +0200

    wygląd B)

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
-from .models import User, Points
+from .models import User, Points, Rank, Note
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db  ##means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -24,32 +24,136 @@ def grafika():
 @auth.route('/progress')
 @login_required
 def progress():
+    CU_name=current_user.first_name
+    x=None
+    tt1=Points.query.filter_by(data='QUIZ1 - wprowadzenie').join(User).filter_by(first_name=CU_name).order_by(Points.points.desc()).first()
+    if (tt1==x):
+        tt1='Nie brałeś udziału'
+    else:
+        tt1=tt1.points
+    tt2=Points.query.filter_by(data='QUIZ2 - parametry techniczne stron').join(User).filter_by(first_name=CU_name).order_by(Points.points.desc()).first()
+    if (tt2==x):
+        tt2='Nie brałeś udziału'
+    else:
+        tt2=tt2.points
+
+    #### USUWANIE STRINGÓW Z WYNIKÓW ZEBY MÓC ZSUMOWAĆ
+    lista_braku_udzialu_INT=[]
+    lista_braku_udzialu=[tt1,tt2]
+    for i in range(len(lista_braku_udzialu)):
+        if lista_braku_udzialu[i] == 'Nie brałeś udziału':
+            lista_braku_udzialu[i] = 0
+    for element in lista_braku_udzialu:
+        lista_braku_udzialu_INT.append(int(element))
+
+
+    suma = sum(lista_braku_udzialu_INT)
+
+
+    if Rank.query.filter_by(name=CU_name).first()==None:
+        new_rank = Rank(name=current_user.first_name, rank_points=suma)  # Provide the user_id
+        db.session.add(new_rank)
+        db.session.commit()
+    else:
+        Rank.query.filter_by(name=CU_name).update({Rank.rank_points: suma})
+        db.session.commit()
+
+    ####################
+    return render_template('progress.html',tt1=tt1,tt2=tt2, user=current_user)
+
+
+@auth.route('/rank')
+def rank():
+    rankall = Rank.query.order_by(Rank.rank_points.desc()).limit(10).all()
+
+    brakujace_elementy = 10 - len(rankall)
+    if brakujace_elementy > 0:
+        rankall.extend([''] * brakujace_elementy)
+    rank1 = rankall[0]
+    rank2 = rankall[1]
+    rank3 = rankall[2]
+    rank4 = rankall[3]
+    rank5 = rankall[4]
+    rank6 = rankall[5]
+    rank7 = rankall[6]
+    rank8 = rankall[7]
+    rank9 = rankall[8]
+    rank10 = rankall[9]
+    check_your_rank = Rank.query.order_by(Rank.rank_points.desc()).all()
+    rank_plaec_CU = 1
+    for i in range(len(check_your_rank)):
+        if check_your_rank[i].name == current_user.first_name:
+            break
+        else:
+            rank_plaec_CU += 1
+
+    your_rank = Rank.query.filter_by(name=current_user.first_name).first()
+    return render_template('rank.html', rank1=rank1, rank2=rank2, your_rank=your_rank, rank3=rank3, rank4=rank4,
+                           rank5=rank5, rank6=rank6, rank7=rank7, rank8=rank8, rank9=rank9, rank10=rank10,
+                           user=current_user, rank_plaec_CU=rank_plaec_CU)
+
+
+@auth.route('/refresh_rank')
+def refresh_rank():
     CU_name = current_user.first_name
     x = None
     tt1 = Points.query.filter_by(data='QUIZ1 - wprowadzenie').join(User).filter_by(first_name=CU_name).order_by(
         Points.points.desc()).first()
     if (tt1 == x):
-        tt1 = 'Nie brałeś/aś udziału'
+        tt1 = 'Nie brałeś udziału'
     else:
         tt1 = tt1.points
-
     tt2 = Points.query.filter_by(data='QUIZ2 - parametry techniczne stron').join(User).filter_by(first_name=CU_name).order_by(
         Points.points.desc()).first()
     if (tt2 == x):
-        tt2 = 'Nie brałeś/aś udziału'
+        tt2 = 'Nie brałeś udziału'
     else:
         tt2 = tt2.points
 
-    return render_template('progress.html', tt1=tt1, tt2=tt2, user=current_user)
+        #### USUWANIE STRINGÓW Z WYNIKÓW ZEBY MÓC ZSUMOWAĆ
+    lista_braku_udzialu_INT = []
+    lista_braku_udzialu = [tt1, tt2]
+    for i in range(len(lista_braku_udzialu)):
+        if lista_braku_udzialu[i] == 'Nie brałeś udziału':
+            lista_braku_udzialu[i] = 0
+    for element in lista_braku_udzialu:
+        lista_braku_udzialu_INT.append(int(element))
 
+    suma = sum(lista_braku_udzialu_INT)
+
+    if Rank.query.filter_by(name=CU_name).first() == None:
+        new_rank = Rank(name=current_user.first_name, rank_points=suma)  # Provide the user_id
+        db.session.add(new_rank)
+        db.session.commit()
+    else:
+        Rank.query.filter_by(name=CU_name).update({Rank.rank_points: suma})
+        db.session.commit()
+
+    return redirect(url_for('auth.rank'))
 
 @auth.route('/course')
 def course():
     return render_template('course.html', user=current_user)
 
-@auth.route('/forum')
+
+@auth.route('/forum', methods=['GET', 'POST'])
 def forum():
-    return render_template('forum.html', user=current_user)
+
+    if request.method == 'POST':
+        note = request.form.get('note')  # Gets the note from the HTML
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.first_name)  # providing the schema for the note
+            db.session.add(new_note)  # adding the note to the database
+            db.session.commit()
+            flash('Note added!', category='success')
+
+
+    all_notes = Note.query.all()
+
+    return render_template('forum.html', user=current_user, notes=all_notes)
 
 @auth.route('/quizFig')
 def quizFig():
@@ -141,9 +245,9 @@ def wynik():
 
 ###########################
 
-@auth.route('/admin')
-def admin():
-    return render_template('admin.html', user=current_user)
+@auth.route('/adminpanel')
+def adminpanel():
+    return render_template('adminpanel.html', user=current_user)
 
 
 @auth.route('/mainPage')
@@ -197,8 +301,11 @@ def sign_up():
         password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first()
+        name=User.query.filter_by(first_name=first_name).first()
         if user:
             flash('Email already exists.', category='error')
+        elif name:
+            flash('Name already exist.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
